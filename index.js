d09357dba771aa0b935c813e25efd46905aa7bdc
hamsa <hafieekhan@gmail.com>
2022-08-30 09:52:18 +0300

    Completed Assinment

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Add your code here
+// FIRST OPTION OF POST
+// const configurationObject = {
+//     method: 'POST',
+//     headers: {
+//         "Content-type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle",
+//     })
+//   };
+//   fetch("http://localhost:3000/dogs", configurationObject);
+
+
+// // SECOND OPTION example
+// const submitData = {
+//     userName: "Steve",
+//     userEmail: "steve@steve.com",
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(res => res.json)
+//   .then(object => console.log(object))
+//   .catch(function (error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   });
+
+// lab work
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+        .then(res => res.json())
+        .then(object => {
+            document.body.innerHTML = object["id"]
+        })
+        .catch(error => {
+            document.body.innerHTML = error.message
+        });
+    };
+
